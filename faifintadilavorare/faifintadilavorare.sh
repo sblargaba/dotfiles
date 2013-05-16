70c7ed5ef06fb071d5067808f9202e58143b2345
Simone Baratta <Conte91@gmail.com>
2013-05-16 18:00:15 +0200

    Gausssssss

--- faifintadilavorare/faifintadilavorare.sh
+++ faifintadilavorare/faifintadilavorare.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-path+=/compilazione
-linee=`wc -l $path | awk '{print $1}'`
+outfile=${path}/compilazione
+linee=`wc -l $outfile | awk '{print $1}'`
 divider=200
 
 clear
 echo "[`whoami`@`hostname` build]$ make"
 while :
-do numeroriga=`expr $numeroriga + 1`
-  sed -n -e "${numeroriga}p" $path
+do
+  numeroriga=`expr $numeroriga + 1`
+  sed -n -e "${numeroriga}p" $outfile
   if [ $numeroriga -gt $linee ]
   then numeroriga=0
   fi
-  sleep `echo "c=$RANDOM % 100; scale=2; c / $divider" | bc`
+  sleep `python ${path}/gauss.py`
 done
---
+++ faifintadilavorare/gauss.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from random import gauss
+
+divider=1
+
+print(abs(gauss(0, 0.5))/divider)
+
