52723b8e0f9fe5236c5172727ca570f684168d6b
sblargaba <schizzobau@gmail.com>
2013-05-17 20:18:59 +0300

    Update faifintadilavorare.py

--- faifintadilavorare/faifintadilavorare.py
+++ faifintadilavorare/faifintadilavorare.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 from random import gauss
 from os import getcwd
-from time import sleep
+from time import sleep, time
 
 speed = 0.1
 sigma = 0.05
 fname = "compilazione"  # path relative to script
 
-
+start = time()
 with open(getcwd() + "/" + fname, encoding='utf-8') as fin:
-  for line in fin:
-   print(line.rstrip())
-   #print(abs(gauss(0, 0.5))/speed)
-   sleep(abs(gauss(0, sigma)/speed))
-
+  try:
+    while (True):
+      for line in fin:
+        print(line.rstrip())
+        sleep(abs(gauss(0, sigma)/speed))
+      fin.seek(0)
+  except KeyboardInterrupt:
+    total = int(time() - start)
+    print("\nTotal run time: {0} hours, {1} minutes and {2} seconds".format(
+      total//3600, total//60, int(total)))
